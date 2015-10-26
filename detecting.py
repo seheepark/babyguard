3fe115017570edc4641f7effb68c57bc5c17a3d8
seheepark <pcsehee@daum.net>
2015-10-26 23:22:56 +0900

    Update detecting.py

--- detecting.py
+++ detecting.py
@@ -68,7 +68,7 @@ while True:
 
 		totalTimer = (endTimer - startTimer) / 600 # Check temp-hum data every 10minutes.
 
-		if 24 <= temperature and temperature <= 26:
+		if 22 <= temperature and temperature <= 24:
 			print 'Temperature is appropriate'
 
 			tempIn = 0
