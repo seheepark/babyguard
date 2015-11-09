b8446c54f7c7dfdccf24abbda86eadf741538c72
seheepark <pcsehee@daum.net>
2015-11-10 00:07:18 +0900

    Update saveDB.py

--- saveDB.py
+++ saveDB.py
@@ -3,7 +3,7 @@ import MySQLdb
 import sys
 import os
 import time
-
+# For use DB
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(("", 5001))
 server_socket.listen(10)
@@ -19,7 +19,7 @@ print("use babysleep")
 while 1:
 	client_socket, address = server_socket.accept()
 	print ("connect from", address)
-	buffer = client_socket.recv(20000)
+	buffer = client_socket.recv(20000) # Receive sleeping data from server
 	print buffer
 	data1 = buffer.split ('.')
 	date = data1[0]
@@ -33,14 +33,14 @@ while 1:
 	result = cursor.fetchall()
 	total = cursor.rowcount
 
-	if total > 100:
+	if total > 100: # If index is over 100, reset the table
 		str = "delete from infosleep"
 		cursor.execute(str)
 		db.commit()
 		str = "alter table infosleep auto_increment=1"
 		cursor.execute(str)
 		db.commit()
-
+	# Insert baby's sleeping data
 	str = "insert into infosleep(date, hour, min, sec, wakeUpCnt) values(%s, %d, %d, %d, %d)" % (date, hour, min, sec, wakeUpCnt)
 	cursor.execute(str)
 	db.commit()
