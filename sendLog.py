53e7684f988406ebfdd143147ebc05ce727d3c71
seheepark <pcsehee@daum.net>
2015-11-20 21:40:16 +0900

    db_date

--- sendLog.py
+++ sendLog.py
@@ -6,25 +6,32 @@ import os
 server_socket = socket (AF_INET, SOCK_STREAM)
 server_socket.bind (("", 5000))
 server_socket.listen(5)
-# For use DB
-db = MySQLdb.connect('localhost', 'root', 'asdf', 'babysleep')
-print("db connect")
-cursor=db.cursor()
-cursor.execute("show databases")
-print("show db")
-cursor.execute("use babysleep")
-print("use babysleep")
 
 while 1:
 	client_socket, address = server_socket.accept()
 	print ("Connect from", address)
-	data = client_socket.recv(1024) # Receive signal from Android (Sleeping data log request message)
+	data = client_socket.recv(1024)
 	print data
 	tmp = '0'	
 	if data == '1':
 		print ("Now, Sending data to Android")
-		cursor.execute ("select * from infosleep") # Extract all data from DB
+		db = MySQLdb.connect ('localhost', 'root', 'asdf', 'babysleep')
+		cursor = db.cursor()
+		cursor.execute ("use babysleep")
+		cursor.execute ("select * from infosleep")
 		row = cursor.fetchall()
+			
+		#db2 = MySQLdb.connect ('localhost', 'root', 'asdf', 'babysleep')
+		#cursor2 = db2.cursor()
+		str2 = "select * from infosleep ORDER BY idx"
+		cursor.execute(str2)
+		result = cursor.fetchall()	
+		total = cursor.rowcount
+
+		if total == 1:
+			tmp = '0'
+			print "Total is 1"
+
 		for c in row:
 			date = c[0]
 			hour = c[2]
@@ -32,13 +39,15 @@ while 1:
 			sec = c[4]
 			wakeUpcnt = c[5]
 		
-			if date != tmp: # If date is different from previous data
+			if date != tmp:
 				client_socket.send (str(date)+"/")
 				#print date
 				idx = 1
-			elif date==tmp: # If date is same from previous data, skip date sending
+			elif date==tmp:
 				idx=idx+1	
 	
 			tmp = date
 			client_socket.send ('%d. %dhour %dmin %dsec. %dwake/' % (idx, hour, min, sec, wakeUpcnt))	
+			#print "%d. %dhour %dmin %dsec %dwake"  % (idx, hour, min, sec, wakeUpcnt)  
+		db.commit()
 		client_socket.close ()
